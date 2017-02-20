19ab4b8bba1ae0b44d8e63a9d4944d0908cdfaec
yurun00 <yurun00@gmail.com>
2017-02-19 18:47:08 -0800

    Plot loss

--- plotFig.py
+++ plotFig.py
@@ -1,6 +1,24 @@
 import pprint, pickle
 import matplotlib.pyplot as plt
 
+def plotLoss():
+    test_result = []
+    val_result = []
+    for t in [0.25, 0.5, 1, 2, 4, 8, 16]:
+        pkl_file = open('bonus_history_t{}'.format(t), 'rb')
+        history = pickle.load(pkl_file)
+        pkl_file.close()
+        test_result.append(history['loss'][-1])
+        val_result.append(history['val_loss'][-1])
+    plt.plot([1, 2, 3, 4, 5, 6, 7], test_result)
+    plt.plot([1, 2, 3, 4, 5, 6, 7], val_result)
+    plt.xticks([1, 2, 3, 4, 5, 6, 7], ['0.25', '0.5', '1', '2', '4', '8', '16'])
+    plt.title('loss versus T per category')
+    plt.ylabel('loss')
+    plt.xlabel('T')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
 def plotAccVST():
     test_result = []
     val_result = []
@@ -13,12 +31,13 @@ def plotAccVST():
     plt.plot([1, 2, 3, 4, 5, 6, 7], test_result)
     plt.plot([1, 2, 3, 4, 5, 6, 7], val_result)
     plt.xticks([1, 2, 3, 4, 5, 6, 7], ['0.25', '0.5', '1', '2', '4', '8', '16'])
-    plt.title('accuracy versus number of samples per category')
+    plt.title('accuracy versus T per category')
     plt.ylabel('accuracy')
-    plt.xlabel('number of samples')
+    plt.xlabel('T')
     plt.legend(['train', 'test'], loc='upper left')
     plt.show()
 
 
 if __name__ == '__main__':
+    plotLoss()
     plotAccVST()
